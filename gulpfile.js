ab458d6069e281211a757c9e75d24e21fcf35fcf
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2015-10-06 22:29:24 -0400

    refactor gulp tasks, remove jquery library, update gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,66 @@
-var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var babel = require('babelify');
-var connect = require('gulp-connect');
-var sass = require('gulp-ruby-sass');
-var uglify = require('gulp-uglify');
-var minifycss = require('gulp-minify-css');
-// var watchcss = require('gulp-watch');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    browserify = require('browserify'),
+    babelify = require('babelify'),
+    nunjucksify = require('nunjucksify'),
+    through2 = require('through2'),
+    source = require('vinyl-source-stream'),
+    transform = require('vinyl-transform'),
+    buffer = require('vinyl-buffer'),
+    sass = require('gulp-ruby-sass'),
+    connect = require('gulp-connect');
 
-function compile(watch) {
-  // gulp.src('src/css/base.scss')
-  // .pipe(sass())
-  // .pipe(minifycss())
-  // .pipe(concat("base.min.css"))
-  // .pipe(gulp.dest('dest/css'));
+// set up localhost server - default port is 8080
+gulp.task('connect', function() {
+  connect.server();
+});
 
+//compile and minify css
+gulp.task('sass', function () {
+  gulp.src('src/css/*.scss')
+  .pipe(sass())
+  .pipe(minifycss())
+  .pipe(concat("base.min.css"))
+  .pipe(gulp.dest('dest/css'))
+});
 
-  var bundler = watchify(browserify('./src/js/app/app.js', { debug: true }).transform(babel));
+// bundle js
+gulp.task('browserify',[],function(){
+  return gulp.src('src/js/app.js')
+    .pipe(through2.obj(function (file, enc, next) {
+        browserify(file.path, {
+            debug: true
+        })
+        .transform(babelify)
+        .transform(nunjucksify)
+        .bundle(function (err, res) {
+            if (err) { return next(err);}
+            file.contents = res;
+            next(null, file);
+        });
+    }))
+    .on('error', function (error) {
+        console.log('browserify error');
+        console.log(error);
+        this.emit('end');
+    })
+    .pipe(buffer())
+    .pipe(gulp.dest('dest/js'));
+});
 
-  function rebundle() {
-    bundler.bundle()
-      .on('error', function(err) { console.error(err); this.emit('end'); })
-      .pipe(source('app.js'))
-      .pipe(buffer())
-      .pipe(gulp.dest('./dest/js'));
+// set default tasks to run localhost, compile sass, and bundle js
+gulp.task('default', [
+    'connect',
+    'sass',
+    'browserify'
+], function() {
+  gulp.start('watch');
+});
 
-    gulp.src('src/css/base.scss')
-    .pipe(sass())
-    .pipe(minifycss())
-    .pipe(concat("base.min.css"))
-    .pipe(gulp.dest('dest/css'));
+// watch for changes to css & js
+gulp.task('watch', function() {
+  gulp.watch(['src/css/*'], ['sass']);
+  gulp.watch(['src/js/*'], ['browserify']);
 
-  }
-
-
-  if (watch) {
-    bundler.on('update', function() {
-      console.log('bundling...');
-      rebundle();
-    });
-  }
-
-  rebundle();
-}
-
-function watch() {
-  return compile(true);
-};
-
-// gulp.watch('src/css/base.scss', function() {
-//   gulp.run('sass');
-// });
-
-// gulp.task('sass', function () {
-//   gulp.src('src/css/base.scss')
-//   .pipe(sass())
-//   .pipe(minifycss())
-//   .pipe(concat("base.min.css"))
-//   .pipe(gulp.dest('dest/css'))
-// });
-
-
-gulp.task('connect', function() {connect.server();});
-gulp.task('build', function() { return compile(); });
-gulp.task('watch', function() { return watch(); });
-
-gulp.task('default', ['watch', 'connect']);
+});
